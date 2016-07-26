25d4dc2442918914251f58bc1439c7bde177afda
mattcook12 <matthcook96+github@gmail.com>
2016-07-26 14:24:33 +0100

    POST requests tests

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('/leaderboard', function (request, response) {
 
 app.post('/LEDon', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-			client.query("INSERT INTO fakescores values(5, $1, 4, $2)", ["jim", 21]);
+			client.query("INSERT INTO fakescores values(5, 'Jim', 4, 43)");
 		});
 });
 
