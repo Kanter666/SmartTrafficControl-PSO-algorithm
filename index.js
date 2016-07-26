3da03fb7feddfa41532104bb240c39b23ca98e12
mattcook12 <matthcook96+github@gmail.com>
2016-07-26 09:16:06 +0100

    Database page

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/leaderboard', function (request, response) {
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.render('/db', {results: result.rows} ); }
+       { response.render('db', {results: result.rows} ); }
     });
   });
 })
