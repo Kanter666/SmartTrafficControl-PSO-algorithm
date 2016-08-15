a3b5ee0eb4b71ad95bc695ef3c8716730f06dbba
mattcook12 <matthcook96+github@gmail.com>
2016-08-15 10:24:35 +0100

    Radom serverside

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/random', function(request, response) {
 		  if (err)
 		   { console.error(err); response.send("Error " + err); }
 		  else
-		   { response.json({ message: 'Bear created!' }); }
+		   { response.json({ "id": result }); }
 		});
 	});
 });
