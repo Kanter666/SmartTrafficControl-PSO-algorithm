be8b63e0848409de98e3524e1518d9321381dd1d
mattcook12 <matthcook96+github@gmail.com>
2016-08-16 10:06:14 +0100

    Touch ups

--- index.js
+++ index.js
@@ -35,16 +35,20 @@ app.post('/', function(request, response) {
 });
 
 app.get('/AMTPayList', function(request, response) {
-	if(request.query.id.substr(0,3) === "AI53"){
-		pg.connect(connectionString, function(err, client, done) {
-			client.query('SELECT gameid FROM results', function(err, result) {
-			  done();
-			  if (err)
-			   { console.error(err); response.send("Error " + err); }
-			  else
-			   { response.json(result.rows); }
+	if(request.query.id !== undefined){
+		if(request.query.id.substr(0,3) === "AI53"){
+			pg.connect(connectionString, function(err, client, done) {
+				client.query('SELECT gameid FROM results', function(err, result) {
+				  done();
+				  if (err)
+				   { console.error(err); response.send("Error " + err); }
+				  else
+				   { response.json(result.rows); }
+				});
 			});
-		});
+		}
+	}else{
+		response.json([]);
 	}
 });
 
